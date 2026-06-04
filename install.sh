7e00c3f3dc8200a0688e44e403f9f5dd6a37aed5
i_use arch_btw <kotrpa0192@gmail.com>
2026-06-04 13:04:18 +0400

    Add installation script for system setup
    Install necessary packages and configure environment.

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo pacman -Sy kitty imagemagick fastfetch dunst polybar sxhkd bspwm rofi cava feh firefox sddm
+mv .xprofile ~/
+mv wallpapers.jpg ~/
+mkdir -p ~/.config/bspwm ~/.config/sxhkd ~/.config/kitty ~/.config/polybar ~/.config/cava ~/.config/fastfetch
+mv bpswm/bspwmrc ~/.config/bspwm/
+mv cava/config ~/.config/cava/
+mv fastfetch/config.jsonc fastfetch/nyarch.png ~/.config/fastfetch/
+mv kitty/kitty.conf ~/.config/kitty/
+mv polybar/config.ini polybar/launch.sh ~/.config/polybar/
+mv sxhkd/sxhkdrc ~/.config/sxhkd/
+sudo systemctl enable sddm
