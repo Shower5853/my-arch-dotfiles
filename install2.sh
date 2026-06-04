4a1d0af9b58e6782f704b9207556574f0ee526f8
i_use arch_btw <kotrpa0192@gmail.com>
2026-06-04 13:15:26 +0400

    Update and rename install.sh to install2.sh

--- install2.sh
+++ install2.sh
@@ -1,4 +1,3 @@
-sudo pacman -Sy kitty imagemagick fastfetch dunst polybar sxhkd bspwm rofi cava feh firefox sddm
 mv .xprofile ~/
 mv wallpapers.jpg ~/
 mkdir -p ~/.config/bspwm ~/.config/sxhkd ~/.config/kitty ~/.config/polybar ~/.config/cava ~/.config/fastfetch
@@ -8,4 +7,3 @@ mv fastfetch/config.jsonc fastfetch/nyarch.png ~/.config/fastfetch/
 mv kitty/kitty.conf ~/.config/kitty/
 mv polybar/config.ini polybar/launch.sh ~/.config/polybar/
 mv sxhkd/sxhkdrc ~/.config/sxhkd/
-sudo systemctl enable sddm
